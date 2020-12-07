90cdd3878ad1ccdb7f2ec7262a7b574911379b86
sergio-vasquez537 <sa.vasquez537@gmail.com>
2020-12-07 09:07:54 -0500

    Updated network_rescale and test

--- R/network_rescale.R
+++ R/network_rescale.R
@@ -1,36 +1,43 @@
 #' @title Rescaling function customized for adjacency matrices
-#' @description This function rescales to a 0 to 1 scale using the values
+#' @description This function rescales to a 0 to 100 scale using the values
 #' present in the matrix. It is based on the rescale function of the
-#' scales package.
+#' scales package. It will make the matrix symmetric by mirroring the upper
+#' half if it isn't. If negative values present, their absolute value will be
+#' taken.
 #' @param adj_mat an adjacency matrix to normalize.
 #' @param to interval in which transformation should be done.
 #' Default is \code{to = c(0, 100)}.
 #' @author Sergio Vasquez and Hajk-Georg Drost
 #' @export
 #' @examples
-#' # path to GENIE3 output file
-#' genie3_output <- system.file('beeline_examples/GENIE3/outFile.csv', package = 'edgynode')
+#' # path to ppcor output file
+#' ppcor_output <- system.file('beeline_examples/PPCOR/outFile.txt', package = 'edgynode')
 #' # parsing the output to an adjacency matrix
-#' genie3_parsed <- genie(genie3_output)
+#' ppcor_parsed <- ppcor(ppcor_output)
 #' # rescaling the matrix
-#' rescaled <- network_rescale(genie3_parsed)
+#' rescaled <- network_rescale(ppcor_parsed)
 #' # Visualize result
 #' head(rescaled)
 
 network_rescale <- function (adj_mat, to = c(0, 100)) {
-  mat_adj <- as.matrix(adj_mat[ , 2:ncol(adj_mat)])
-  diag(mat_adj) <- 0
+  
+  if (is.character(adj_mat[1,1]) == TRUE)
+    warning("The first column consists of names.")
+    #adj_mat <- as.matrix(adj_mat[ , 2:ncol(adj_mat)])
+  
+  diag(adj_mat) <- 0
 
-  if (!isSymmetric(mat_adj))
-    warning("Please provide a symmetric matrix as 'adj_mat' input for network_rescale().", call. = FALSE)
+  if (!isSymmetric(adj_mat))
+    warning("The matrix provided as input for network_rescale() was coerced into symmetric.")
+    adj_mat <- make_symmetric(as.matrix(adj_mat))
 
-  if (dplyr::between(min(mat_adj), -1L, -0.8) && dplyr::between(max(mat_adj), 0.8, 1L))
+  if (dplyr::between(min(adj_mat), -1L, -0.8) && dplyr::between(max(adj_mat), 0.8, 1L))
     warning("It seems like your input matrix contains values of correlation coefficients range(-1,1).",
-            " Please be aware that scaling these values between [0,100] means that a rescaled value of 50 encodes corellation values of 0 (uncorrelated) which may make interpretations more difficult.")
-
-  rescaled_mat <- scales::rescale(mat_adj, to = to)
-  result <- tibble::as_tibble(cbind(adj_mat[ , 1], rescaled_mat))
-  result <- as.matrix(result)
+            " Please be aware that for negative values the absolute value will be taken before rescaling.")
+    adj_mat <- abs(adj_mat)
+    
+  rescaled_mat <- scales::rescale(adj_mat, to = to)
+  result <- as.matrix(rescaled_mat)
   
   return(result)
 }
--- tests/testthat/test-network_rescale.R
+++ tests/testthat/test-network_rescale.R
@@ -8,11 +8,13 @@ test_that("the rescale adjacency matrix function is working correctly", {
   pidc_parsed <- pidc(pidc_output)
   # rescaling the matrix
   rescaled <- network_rescale(pidc_parsed)
-  # Visualize result
+  # result
   expect_output(print(rescaled))
+  # symmetry
+  expect_true(isSymmetric(rescaled))
 
-  expect_equal(nrow(rescaled), 18)
-  expect_equal(ncol(rescaled), 18)
-  expect_true(all(apply(rescaled[2:18], 2, is.numeric)))
+  expect_equal(nrow(rescaled), 19)
+  expect_equal(ncol(rescaled), 19)
+  expect_true(all(apply(rescaled, 2, is.numeric)))
 
 })
