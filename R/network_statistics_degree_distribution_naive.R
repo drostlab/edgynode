4f80bad6cf603b95966f721ea259af5a5ba10d52
HajkD <hajkdrost@googlemail.com>
2021-01-11 21:17:26 +0100

    add test whether or not input matrix is a binary adjacency matrix

--- R/network_statistics_degree_distribution_naive.R
+++ R/network_statistics_degree_distribution_naive.R
@@ -25,11 +25,14 @@ network_statistics_degree_distribution_naive <- function (adj_mat) {
         "Please provide a symmetric matrix as 'adj_mat' input for network_statistics().",
         call. = FALSE
       )
+  
+    if (!is_binary_matrix(adj_mat))
+      stop("Please make sure to insert a binary adjacency matrix, e.g. using edgynode::network_make_binary().", call. = FALSE)
     
     #distributions and cohesive blocks need graph objects
-  
     res_row <- sort(rowSums(adj_mat))
     res_col <- sort(colSums(adj_mat))
+    
     if(!all(res_row == res_col))
       stop("It seems like rowSums and colSums are not equal. Please check what could have gone wrong.", call. = FALSE)
     res <- tibble::tibble(gene_name = names(res_row), node_degree = as.integer(res_row))
