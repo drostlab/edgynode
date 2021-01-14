76d26f3b8c6c254903e4932babb2e6495139f553
HajkD <hajkdrost@googlemail.com>
2021-01-14 17:03:03 +0100

    change default sizes

--- R/plot_network_weight_distribution_boxplot.R
+++ R/plot_network_weight_distribution_boxplot.R
@@ -9,9 +9,9 @@
 #' The threshold can either be a numeric balue in the interval [0,100] or a character string
 #' specifying the following methods for automatically determining the threshold based on the input data:
 #' \itemize{
-#' \item \code{threshold = "median"}: compute the \code{\link{median}} over the entire input \code{adj_mat} and use this 
+#' \item \code{threshold = "median"}: compute the \code{\link{median}} over the entire input \code{adj_mat} and use this
 #' \code{median} value as threshold for defining all edge weights of a genes equal or below the \code{median}
-#' value as \code{0} and all values above the \code{median} value as \code{1}.  
+#' value as \code{0} and all values above the \code{median} value as \code{1}.
 #' }
 #' The threshold value will then be drawn as vertical line in the plot.
 #' @author Sergio Vasquez and Hajk-Georg Drost
@@ -36,36 +36,28 @@ plot_network_weight_distribution_boxplot <-
     
     value <- name <- NULL
     
-    if (is.numeric(threshold)) {
-      p <-
-        ggplot2::ggplot(adj_mat_long, ggplot2::aes(x = value, y = name, colour = value)) + 
-        ggplot2::geom_point(ggplot2::aes(y = name, color = value), size = .5, alpha = 0.8) +
-        ggplot2::geom_boxplot(alpha = 0.5) +
-        ggplot2::xlab(xlab) + ggplot2::ylab(ylab) + ggplot2::geom_vline(
-          xintercept = threshold,
-          color = "red",
-          size = 1.5,
-          alpha = 0.3
-        ) +
-        ggplot2::scale_colour_continuous(high = "#132B43", low = "#56B1F7")
+    if (threshold == "median") {
+      threshold <- round(median(adj_mat) , 2)
     }
     
-    
-    if (is.character(threshold)) {
-      if (threshold == "median"){ 
-        p <-
-          ggplot2::ggplot(adj_mat_long, ggplot2::aes(x = value, y = name, colour = value)) + 
-          ggplot2::geom_point(ggplot2::aes(y = name, color = value), size = .5, alpha = 0.8) +
-          ggplot2::geom_boxplot(alpha = 0.5) +
-          ggplot2::xlab(xlab) + ggplot2::ylab(ylab) + ggplot2::geom_vline(
-            xintercept = round(median(adj_mat) , 2),
-            color = "red",
-            size = 1.5,
-            alpha = 0.3
-          ) +
-          ggplot2::scale_colour_continuous(high = "#132B43", low = "#56B1F7")
-      }
-    }
+    p <-
+      ggplot2::ggplot(adj_mat_long, ggplot2::aes(x = value, y = name, colour = value)) +
+      ggplot2::geom_point(
+        ggplot2::aes(y = name, color = value),
+        size = .5,
+        alpha = 0.4,
+        position = ggplot2::position_jitter(width = 0, height = 0.28)
+      ) +
+      ggplot2::geom_boxplot(fill = NA,
+                            outlier.shape = NA,
+                            size = 0.3) +
+      ggplot2::xlab(xlab) + ggplot2::ylab(ylab) + ggplot2::geom_vline(
+        xintercept = threshold,
+        color = "red",
+        size = 1.5,
+        alpha = 0.3
+      ) +
+      ggplot2::scale_colour_continuous(high = "#132B43", low = "#56B1F7")
     
     return(p)
   }
