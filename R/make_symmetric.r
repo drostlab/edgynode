e68bc19dff35cd94bc1ba5ea002106087216c851
HajkD <hajkdrost@googlemail.com>
2021-01-15 21:16:13 +0100

    rename to network_make_symmetric for consistency

--- R/make_symmetric.r
+++ R/make_symmetric.r
@@ -1,3 +1,8 @@
-#Function to force a matrix into being symmetric
+#' @title Force a matrix into being symmetric
+#' @description This function takes and \code{adj_mat} as input and forces it into being symmetric.
+#' @param adj_mat an adjacency matrix that shall be made symmetric.
+#' @author lmshk
+#' @export
 
-make_symmetric <- function(uneven) pmax(uneven, t(uneven))
+network_make_symmetric <- function(adj_mat)
+  pmax(adj_mat, t(adj_mat))
