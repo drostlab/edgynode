866ba28ee6689f706a7d0d1da956862761ad9114
HajkD <hajkdrost@googlemail.com>
2021-01-15 21:18:36 +0100

    update example

--- R/plot_network_weight_distribution_boxplot.R
+++ R/plot_network_weight_distribution_boxplot.R
@@ -16,14 +16,14 @@
 #' The threshold value will then be drawn as vertical line in the plot.
 #' @author Sergio Vasquez and Hajk-Georg Drost
 #' @examples
-#' # path to PPCOR output file
-#' ppcor_output <- system.file('beeline_examples/PPCOR/outFile.txt', package = 'edgynode')
-#' # import PPCOR output into adjacency matrix
-#' ppcor_parsed <- ppcor(ppcor_output)
+#' genie3_49_raw <- as.matrix(read.csv(
+#' system.file("data/network_raw_49_placenta_development.csv",
+#'  package = "edgynode"), row.names = 1))
 #' # rescaling weighted adjacency matrix to range [0,100]
-#' ppcor_rescaled <- network_rescale(ppcor_parsed)
+#' genie_rescaled <- network_rescale(genie3_49_raw)
 #' # visualize weight distributions over all edges for each gene
-#' plot_network_weight_distribution_boxplot(ppcor_rescaled, threshold = 70)
+#' # with median threshold
+#' plot_network_weight_distribution_boxplot(genie_rescaled, threshold = "median")
 #' @export
 
 plot_network_weight_distribution_boxplot <-
@@ -43,21 +43,29 @@ plot_network_weight_distribution_boxplot <-
     p <-
       ggplot2::ggplot(adj_mat_long, ggplot2::aes(x = value, y = name, colour = value)) +
       ggplot2::geom_point(
-        ggplot2::aes(y = name, color = value),
-        size = .3,
+        size = 0.1,
         alpha = 0.4,
+        colour = "black",
         position = ggplot2::position_jitter(width = 0, height = 0.28)
       ) +
-      ggplot2::geom_boxplot(fill = NA,
-                            outlier.shape = NA,
-                            size = 0.3) +
+      ggplot2::geom_boxplot(
+        outlier.shape = NA,
+        size = 0.3,
+        colour = "darkgray",
+        fill = NA
+      ) +
       ggplot2::xlab(xlab) + ggplot2::ylab(ylab) + ggplot2::geom_vline(
         xintercept = threshold,
         color = "red",
         size = 0.6,
         alpha = 0.3
       ) +
-      ggplot2::scale_colour_continuous(high = "#132B43", low = "#56B1F7")
+      ggplot2::theme(
+        panel.background = ggplot2::element_rect(fill = NA),
+        axis.title       = ggplot2::element_text(size = 16),
+        axis.text.y      = ggplot2::element_text(size = 4),
+        axis.text.x      = ggplot2::element_text(size = 16),
+      )
     
     return(p)
   }
