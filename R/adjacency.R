ba7b5343c21167ce57def7f3b042d76defe8c0ff
Ilias Moutsopoulos <emouts@hotmail.com>
2022-07-01 14:40:59 +0100

    Created adjacency class

---
+++ R/adjacency.R
@@ -0,0 +1,25 @@
+make_adjacency <- function(x, known_symmetric = FALSE, known_binary = FALSE){
+  if(is_adjacency(x)) stop("Matrix is already an adjacency")
+  if(!is.matrix(x)) stop("Please supply a matrix")
+  if(nrow(x) != ncol(x)) stop("Matrix must be square")
+  if(is.null(rownames(x)) | is.null(colnames(x))) stop("Matrix must have row/column names")
+  if(any(rownames(x) != colnames(x))) stop("Matrix row and column names must match")
+  if(!is.numeric(x)) stop("Matrix must have numeric values")
+  adj <- structure(x, class = c("adjacency", class(x)))
+  attr(adj, "known_symmetric") <- known_symmetric
+  attr(adj, "known_binary") <- known_binary
+  adj
+}
+
+is_adjacency <- function(x){
+  all(
+    inherits(x, "adjacency"),
+    is.matrix(x),
+    nrow(x) == ncol(x),
+    !(is.null(rownames(x)) | is.null(colnames(x))),
+    all(rownames(x) == colnames(x)),
+    is.numeric(x),
+    !is.null(attr(x, "known_symmetric")),
+    !is.null(attr(x, "known_binary"))
+  )
+}
