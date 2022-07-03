87f900deb5a397ba68e69c418108c5a956576b63
HajkD <hajkdrost@googlemail.com>
2022-07-03 19:43:14 +0200

    add documentation and examples to make_adjacency()

--- R/make_adjacency.R
+++ R/make_adjacency.R
@@ -1,3 +1,24 @@
+#' @title Convert a raw input matrix into an \pkg{edgynode} adjacency matrix
+#' @description Users may work with a vast variety of input matrices that 
+#' are derived from various gene regulatory network inference methods and tools
+#' with various values and value ranges. This function aims to ensure that 
+#' such raw data is converted into a \pkg{edgynode} adjacency matrix format
+#' and is the first conversion step when importing existing \code{raw} adjacency matrices 
+#' into the \pkg{edgynode} ecosystem.
+#' @param x a \code{\link{matrix}} object.
+#' @param known_symmetric a logical value indicating whether the input matrix \code{x} is already known to be symmetric
+#' (for this case use \code{known_symmetric = TRUE}) or whether the input shall be converted internally into a symmetric matrix
+#' (for this case use \code{known_symmetric = FALSE} (default)).
+#' @param known_binary a logical value indicating whether the input matrix \code{x} is already known to be a binary matrix
+#' (for this case use \code{known_binary = TRUE}) or whether the input shall be converted internally into a binary matrix using \code{\link{make_binary}}
+#' (for this case use \code{known_binary = FALSE} (default)).
+#' @examples 
+#' # look at raw matrix
+#' edgynode::adjacency_matrix_test_3
+#' # convert raw matrix into a edgynode adjacency matrix
+#' edgynode::make_adjacency(edgynode::adjacency_matrix_test_3)
+#' @seealso \code{\link{make_symmetric}}, \code{\link{make_standard}}, \code{\link{make_binary}}
+#' @export
 make_adjacency <- function(x, known_symmetric = FALSE, known_binary = FALSE){
   if(is_adjacency(x)) stop("Matrix is already an adjacency")
   if(!is.matrix(x)) stop("Please supply a matrix")
