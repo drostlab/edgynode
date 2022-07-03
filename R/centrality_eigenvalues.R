6d4589638e7f96c3b0547cb7182da4e070b78dc1
sergio-vasquez537 <sa.vasquez537@gmail.com>
2022-07-03 19:15:12 +0200

    Add centrality functions

---
+++ R/centrality_eigenvalues.R
@@ -0,0 +1,16 @@
+#Calculate eigenvectors as centrality measure
+
+centrality_eigenvalues <- function(adj_mat){
+  #First convert from matrix to igraph object
+  adj_mat[is.na(adj_mat)] <- 0
+  adj_mat <- as.matrix(adj_mat)
+  
+  if(all(adj_mat %in% 0:1)){
+    adj_mat <- igraph::graph.adjacency(adj_mat)
+  }
+  else {adj_mat <- igraph::graph_from_adjacency_matrix(adj_mat, weighted = T)}
+  
+  node_eigenvalues <- igraph::eigen_centrality(adj_mat)
+  
+  return(node_eigenvalues$vector)
+}
