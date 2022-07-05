d8ae498365cdf7f31c17ee98557c1d5a4aef1b9b
Ilias Moutsopoulos <emouts@hotmail.com>
2022-07-05 17:10:37 +0100

    Updated DeltaCon documentation

--- R/summary_delta_con_similarity.R
+++ R/summary_delta_con_similarity.R
@@ -1,12 +1,13 @@
-#' Calculate graph similarity based on DeltaCon
-#'
+#' @title Calculate DeltaCon similarity
+#' @description
 #' Computes the similarity matrix of an adjacency based on the DeltaCon method.
 #' Adapted from previous implementation by Baoxu(Dash) Shi, Data Sciense Group,
 #' University of Notre Dame
-#'
-#' @references D. Koutra, J. T. Vogelstein, and C. Faloutsos: DeltaCon: A Principled Massive-Graph Similarity Function. SIAM 2013: 162–170.
-#' @references D. Koutra, T. Ke, U. Kang, D. H. Chau, H. K. Pao, C. Faloutsos: Unifying Guilt-by-Association Approaches: Theorems and Fast Algorithms. ECML/PKDD (2) 2011: 245-260
-#'
+#' @references D. Koutra, J. T. Vogelstein, and C. Faloutsos: DeltaCon:
+#' A Principled Massive-Graph Similarity Function. SIAM 2013: 162-170.
+#' @references D. Koutra, T. Ke, U. Kang, D. H. Chau, H. K. Pao, C. Faloutsos:
+#' Unifying Guilt-by-Association Approaches: Theorems and Fast Algorithms.
+#' ECML/PKDD (2) 2011: 245-260
 #' @param adj the adjacency matrix
 #' @param degrees the vector of node degrees
 #' @param .MAX_POWER maximum power for matrix inversion
@@ -77,38 +78,3 @@ summary_delta_con_similarity <- function(
 
   inv_ * 0.01
 }
-
-# delta_con_example <- function() {
-#   g1 <- as.data.frame(cbind(c(1,1,2,2,3,3,4,8,5),
-#                             c(2,3,4,5,6,7,8,9,10)))
-#   g2 <- as.data.frame(cbind(c(1,1,2,2,3,3,4,8,5,9,10,5,6),
-#                             c(2,3,4,5,6,7,8,9,10,11,11,12,12)))
-#
-#   print(delta_con(g1,g1,max(g1,g2), debug = TRUE))
-#   print(delta_con(g1,g2,max(g1,g2), debug = TRUE))
-#   print(delta_con(g2,g1,max(g1,g2), debug = TRUE))
-#   print(delta_con(g2,g2,max(g1,g2), debug = TRUE))
-#
-#   print(delta_con(g2,g1,max(g1,g2), method="fast", debug = TRUE))
-#   print(delta_con(g2,g2,max(g1,g2), method="fast", debug = TRUE))
-#
-#   print(delta_con(g1,g1,max(g1,g2), method="fast", symmetrical = FALSE))
-#   print(delta_con(g1,g2,max(g1,g2), method="fast", symmetrical = FALSE))
-#   print(delta_con(g2,g1,max(g1,g2), method="fast", symmetrical = FALSE))
-#   print(delta_con(g2,g2,max(g1,g2), method="fast", symmetrical = FALSE))
-#
-#   print(delta_con(g1,g1,max(g1,g2), symmetrical = FALSE))
-#   print(delta_con(g1,g2,max(g1,g2), symmetrical = FALSE))
-#   print(delta_con(g2,g1,max(g1,g2), symmetrical = FALSE))
-#   print(delta_con(g2,g2,max(g1,g2), symmetrical = FALSE))
-#
-#   g3 <- as.data.frame(cbind(c(2,3),
-#                             c(1,1)))
-#   g4 <- as.data.frame(cbind(c(2,3),
-#                             c(4,4)))
-#
-#   g5 <- as.data.frame(cbind(c(2,3,3,1,5,6,6),
-#                             c(1,1,2,4,4,4,5)))
-#   g6 <- as.data.frame(cbind(c(2,3,3,5,6,6),
-#                             c(1,1,2,4,4,5)))
-# }
