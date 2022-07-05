b29cb7affc0992cb81a2c54aa8a16a710148ae75
Ilias Moutsopoulos <emouts@hotmail.com>
2022-07-05 17:02:39 +0100

    Updated *_adjacency() functions setup and documentation

--- R/assert_adjacency.R
+++ R/assert_adjacency.R
@@ -1,4 +1,34 @@
+#' @title Check whether a matrix fulfills the \pkg{edgynode} adjacency
+#' matrix convention
+#' @description Imports a matrix and tests whether it is an adjacency in
+#' \pkg{edgynode} format.
+#' @param x a \code{\link{matrix}} object.
+#' @return An informative error if the test fails, NULL otherwise
+#' @examples
+#' # test with an already clean adjacency matrix
+#' edgynode::assert_adjacency(edgynode::adjacency_clean_test_3)
+#'
+#' # look at raw matrix
+#' edgynode::adjacency_matrix_test_3
+#' # convert raw matrix into a edgynode adjacency matrix
+#' clean_matrix <- edgynode::make_adjacency(edgynode::adjacency_matrix_test_3)
+#' # test converted matrix
+#' edgynode::assert_adjacency(clean_matrix)
+#' @author Ilias Moutsopoulos and Hajk-Georg Drost
+#' @seealso \code{\link{make_adjacency}}, \code{\link{is_adjacency}}
+#' @export
 assert_adjacency <- function(x){
-  if(!is_adjacency(x)) stop(deparse(substitute(x)), " must be an adjacency matrix with numeric values. You can convert your input matrix using edgynode::make_adjacency().")
+  error_message_1 <- paste(deparse(substitute(x)), "is not an adjacency ")
+  error_message_3 <- "\n  You can convert an input matrix using edgynode::make_adjacency()."
+  full_error_message <- function(str) paste0(error_message_1, "(", str, "). ", error_message_3)
+  if(!inherits(x, "adjacency")) stop(full_error_message("not of class 'adjacency'"))
+  if(!is.matrix(x)) stop(full_error_message("not a matrix"))
+  if(nrow(x) != ncol(x)) stop(full_error_message("different # of rows and columns"))
+  if(is.null(rownames(x))) stop(full_error_message("no row names"))
+  if(is.null(colnames(x))) stop(full_error_message("no column names"))
+  if(!all(rownames(x) == colnames(x))) stop(full_error_message("row/column names don't match"))
+  if(!is.numeric(x)) stop(full_error_message("non-numeric values"))
+  if(is.null(attr(x, "known_symmetric"))) stop(full_error_message("missing known_symmetric attribute"))
+  if(is.null(attr(x, "known_binary"))) stop(full_error_message("missing known_binary attribute"))
   invisible(NULL)
 }
--- R/is_adjacency.R
+++ R/is_adjacency.R
@@ -1,10 +1,11 @@
-#' @title Check whether a \code{\link{make_adjacency}} converted matrix fulfills  the \pkg{edgynode} adjacency matrix convention
-#' @description Imports a matrix and tests whether it is an adjacency in \pkg{edgynode} format.
+#' @title Check whether a matrix fulfills  the \pkg{edgynode}
+#' adjacency matrix convention
+#' @description Tests whether a matrix is an adjacency in \pkg{edgynode} format.
 #' @param x a \code{\link{matrix}} object.
-#' @examples 
+#' @return The result of the test (TRUE/FALSE)
+#' @examples
 #' # test with an already clean adjacency matrix
 #' edgynode::is_adjacency(edgynode::adjacency_clean_test_3)
-#' 
 #' # look at raw matrix
 #' edgynode::adjacency_matrix_test_3
 #' # convert raw matrix into a edgynode adjacency matrix
@@ -12,17 +13,8 @@
 #' # test converted matrix
 #' edgynode::is_adjacency(clean_matrix)
 #' @author Ilias Moutsopoulos and Hajk-Georg Drost
-#' @seealso \code{\link{is_symmetric}}, \code{\link{is_standard}}, \code{\link{is_binary}}
+#' @seealso \code{\link{make_adjacency}}, \code{\link{assert_adjacency}}
 #' @export
 is_adjacency <- function(x){
-  all(
-    inherits(x, "adjacency"),
-    is.matrix(x),
-    nrow(x) == ncol(x),
-    !(is.null(rownames(x)) | is.null(colnames(x))),
-    all(rownames(x) == colnames(x)),
-    is.numeric(x),
-    !is.null(attr(x, "known_symmetric")),
-    !is.null(attr(x, "known_binary"))
-  )
+  !inherits(tryCatch(assert_adjacency(x), error = function(e) e), "error")
 }
