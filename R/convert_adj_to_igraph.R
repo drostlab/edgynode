b108c80b049732941bf06f559d65553caf97bba6
sergio-vasquez537 <sa.vasquez537@gmail.com>
2023-11-15 16:21:27 +0100

    Added documentation

--- R/convert_adj_to_igraph.R
+++ R/convert_adj_to_igraph.R
@@ -1,3 +1,10 @@
+#' @title Convert an adjacency matrix to igraph object
+#' @description This function takes an adjacency matrix and
+#' transforms it into an igraph object
+#' @param adj the adjacency matrix
+#' @author Ilias Moutsopoulos and Sergio Vasquez
+#' @export
+
 convert_adj_to_igraph <- function(adj){
   assert_known_standard(adj)
   if(attr(adj, "known_binary")){
--- R/convert_adj_to_matrix.R
+++ R/convert_adj_to_matrix.R
@@ -1,3 +1,10 @@
+#' @title Convert an adjacency matrix to plain matrix
+#' @description This function takes an adjacency matrix and
+#' transforms it into a plain matrix
+#' @param adj the adjacency matrix
+#' @author Ilias Moutsopoulos and Sergio Vasquez
+#' @export
+
 convert_adj_to_matrix <- function(adj){
   assert_adjacency(adj)
   x <- adj[]
