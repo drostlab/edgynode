b1f276879dba3acb70e1c4d5cbeffcd270e5644d
sergio-vasquez537 <sa.vasquez537@gmail.com>
2023-11-15 19:25:35 +0100

    Added documentation

--- R/summary_centrality_edge_shortest_paths.R
+++ R/summary_centrality_edge_shortest_paths.R
@@ -1,4 +1,11 @@
-#Function to calculate the closeness of edges and nodes
+#' @title Function to calculate betweeness in a network
+#' @description This function an adjacency matrix as required by edgynode
+#' @param adj, an adjacency matrix with adjacency attributes
+#' @author Sergio Vasquez and Hajk-Georg Drost
+#' @examples
+#' # if the matrix is not yet standard:
+#' summary_centrality_edge_shortest_paths(make_standard(make_adjacency(adj)))
+#' @export
 
 summary_centrality_edge_shortest_paths <- function(adj){
   igmat <- convert_adj_to_igraph(adj)
